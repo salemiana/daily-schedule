e33c422e53edd0f0544194c8135e0a24ae11034d
salem z <salem.zerezghi2@gmail.com>
2022-04-23 20:06:11 -0400

    added some background colors

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//display todays date and time
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    
+    })
+})
